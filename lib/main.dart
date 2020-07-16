2478962aa3cd7f420ed8a22567dff6cd9d047f0f
Jeysel Pacheco Bastos <jeysel@gmail.com>
2020-07-16 18:42:59 -0300

    Fim

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/themes/dark-yellow.theme.dart';
+import 'package:shopping/themes/dark.theme.dart';
+import 'package:shopping/themes/light.theme.dart';
+import 'pages/tabs.page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme(),
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
       ),
-      home: HomePage(),
     );
   }
 }
